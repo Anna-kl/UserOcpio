dcf2d24c3d2c44fe9f0ece96c482976afeb47cb8
Аня Климова <klimova_88@mail.ru>
2021-11-24 11:00:25 +0300

    Add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {ModalManager} from "ngb-modal";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {ModalComponent} from "./modal/modal.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'UserOcpio';
+
+  @ViewChild('myModal') myModal: any;
+  private modalRef: any;
+  constructor(private modalService: NgbModal) {}
+
+  open() {
+    const modalRef = this.modalService.open(ModalComponent);
+    modalRef.componentInstance.name = 'World';
+  }
+
+  ngOnInit(): void {
+this.open();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ModalModule} from "ngb-modal";
+import {FormsModule} from "@angular/forms";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {ModalComponent} from "./modal/modal.component";
+import { ModalNextComponent } from './modal-next/modal-next.component';
+import { ModalRegisterComponent } from './modal-register/modal-register.component';
+import { ModalEnterDataComponent } from './modal-enter-data/modal-enter-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent,
+    ModalNextComponent,
+    ModalRegisterComponent,
+    ModalEnterDataComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        NgbModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
