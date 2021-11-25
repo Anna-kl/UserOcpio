edd6337c59ed8c74f49f9768dfeaf32ee0e2c8c6
Аня Климова <klimova_88@mail.ru>
2021-11-25 19:55:27 +0300

    Add project

---
+++ src/app/modal-enter-data/modal-enter-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-enter-data',
+  templateUrl: './modal-enter-data.component.html',
+  styleUrls: ['./modal-enter-data.component.css']
+})
+export class ModalEnterDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modal-register/modal-register.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {NgbActiveModal, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {ModalEnterDataComponent} from "../modal-enter-data/modal-enter-data.component";
+
+@Component({
+  selector: 'app-modal-register',
+  templateUrl: './modal-register.component.html',
+  styleUrls: ['./modal-register.component.css']
+})
+export class ModalRegisterComponent implements OnInit {
+
+  constructor(private modalService: NgbModal, public activeModal: NgbActiveModal) { }
+
+  ngOnInit(): void {
+  }
+
+  next() {
+    this.activeModal.close();
+    const modalRef = this.modalService.open(ModalEnterDataComponent);
+    modalRef.componentInstance.name = 'World';
+  }
+}
